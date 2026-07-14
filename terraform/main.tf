5d9a79b9ef35812ab71c6909ff00f354b7a9bdfb
santhoshbabu0411 <santhoshbabu0411@gmail.com>
2026-07-14 08:41:25 +0530

    main file update1

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 module "azure_vm" {
-  source = "modules/azure-vm"
+  source = "./modules/azure-vm"
 
   resource_group_name  = azurerm_resource_group.main.name
   location             = azurerm_resource_group.main.location
